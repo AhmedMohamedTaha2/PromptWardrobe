7d33c336752cf82ab34a5ed17df5aa417b2f0bf6
AhmedMohamedTaha2 <202115281@o6u.edu.eg>
2026-03-27 13:55:40 +0200

    Fix ESLint react-refresh build error by allowing Route export

--- src/routes/public/prompt-techniques.jsx
+++ src/routes/public/prompt-techniques.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-refresh/only-export-components */
+ 
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { X, Code2 } from "lucide-react";
